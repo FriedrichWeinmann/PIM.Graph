d34a60d4720f0b1794433186b0b6caf49168875c
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2023-02-23 17:35:25 +0100

    Updates

---
+++ PIM.Graph/functions/Stop-PIMRoleRequest.ps1
@@ -0,0 +1,16 @@
+﻿function Stop-PIMRoleRequest {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+		[Alias('RequestID')]
+		[string[]]
+		$ID
+	)
+
+	process {
+		foreach ($requestID in $ID) {
+			try { Invoke-PimGraphRequest -Method POST -Uri "v1.0/roleManagement/directory/roleAssignmentScheduleRequests/$requestID/cancel" -ErrorAction Stop }
+			catch { $PSCmdlet.WriteError($_) }
+		}
+	}
+}
